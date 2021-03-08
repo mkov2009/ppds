95e0687182e16e131bad2dd24cad329566fa6a73
Maros Kovalak <kovalak.maros@gmail.com>
2021-03-08 23:33:07 +0100

    add turnstile for starvation

--- cv3/main.py
+++ cv3/main.py
@@ -26,37 +26,46 @@ class LightSwitch(object):
 class Shared():
     def __init__(self):
         self.room = Semaphore(1)
+        self.turn = Semaphore(1)
 
 
-def writer(thread_id, shared):
-    # pred kazdym pokusom o zapis pocka v intervale <0.0; 1> sekundy
-    sleep(randint(0, 10) / 10)
+def writer(shared):
+    while True:
+        sleep(randint(0, 10) / 10)
 
-    shared.room.wait()
-    print(thread_id)
-    # simulujeme dlzku zapisu v intervale <0.3; 0.7> sekundy
-    sleep(0.3 + randint(0, 4) / 10)
-    shared.room.signal()
+        shared.turn.wait()
+        shared.room.wait()
+        print("W - inside ")
+        sleep(0.3 + randint(0, 4) / 10)
+        shared.room.signal()
+        shared.turn.signal()
+        print("W - after ")
 
 
 def reader(thread_id, shared):
-    lightSwitch.lock(shared.room)
-    sleep(0.3 + randint(0, 4) / 10)
-    print(thread_id)
-    lightSwitch.unlock(shared.room)
+    while True:
+        shared.turn.wait()
+        shared.turn.signal()
+        sleep(randint(0, 10) / 10)
+
+        lightSwitch.lock(shared.room)
+        print("R - inside ")
+        sleep(0.3 + randint(0, 4) / 10)
+        lightSwitch.unlock(shared.room)
+        print("R - after ")
 
 
 shared = Shared()
 lightSwitch = LightSwitch()
 threads = []
 
-for i in range(10):
+for i in range(1):
     t = Thread(writer, f"writer {i}", shared)
     threads.append(t)
 
+for i in range(1):
     t = Thread(reader, f"reader {i}", shared)
     threads.append(t)
-
 
 for t in threads:
     t.join()
