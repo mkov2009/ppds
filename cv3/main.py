edb50a5bdec4c73980f4187621c4ac60df6b10e4
Maros Kovalak <kovalak.maros@gmail.com>
2021-03-08 21:39:38 +0100

    feat: add writer and reader

--- cv3/main.py
+++ cv3/main.py
@@ -1,4 +1,6 @@
-from fei.ppds import Mutex, Semaphore
+from fei.ppds import Mutex, Semaphore, Thread
+from time import sleep
+from random import randint
 
 
 class LightSwitch(object):
@@ -19,3 +21,42 @@ class LightSwitch(object):
         if self.counter == 0:
             semaphore.signal()
         self.mutex.unlock()
+
+
+class Shared():
+    def __init__(self):
+        self.room = Semaphore(1)
+
+
+def writer(thread_id, shared):
+    # pred kazdym pokusom o zapis pocka v intervale <0.0; 1> sekundy
+    sleep(randint(0, 10) / 10)
+
+    shared.room.wait()
+    print(thread_id)
+    # simulujeme dlzku zapisu v intervale <0.3; 0.7> sekundy
+    sleep(0.3 + randint(0, 4) / 10)
+    shared.room.signal()
+
+
+def reader(thread_id, shared):
+    lightSwitch.lock(shared.room)
+    sleep(0.3 + randint(0, 4) / 10)
+    print(thread_id)
+    lightSwitch.unlock(shared.room)
+
+
+shared = Shared()
+lightSwitch = LightSwitch()
+threads = []
+
+for i in range(10):
+    t = Thread(writer, f"writer {i}", shared)
+    threads.append(t)
+
+    t = Thread(reader, f"reader {i}", shared)
+    threads.append(t)
+
+
+for t in threads:
+    t.join()
