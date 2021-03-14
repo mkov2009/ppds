7cc7b10bdaa00f9d4dc035e936db17ff0d4fe377
Maros Kovalak <kovalak.maros@gmail.com>
2021-03-14 17:34:16 +0100

    feat: add lightswitch

---
+++ cv4/main.py
@@ -0,0 +1,23 @@
+from fei.ppds import Mutex
+
+
+class LightSwitch(object):
+    def __init__(self):
+        self.mutex = Mutex()
+        self.counter = 0
+
+    def lock(self, semaphore):
+        self.mutex.lock()
+        counter = self.counter
+        self.counter += 1
+        if self.counter == 1:
+            semaphore.wait()
+        self.mutex.unlock()
+        return counter;
+
+    def unlock(self, semaphore):
+        self.mutex.lock()
+        self.counter -= 1
+        if self.counter == 0:
+            semaphore.signal()
+        self.mutex.unlock()
