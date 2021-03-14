86db0f1f9c258ba210a797d6662fee72cd392f44
Maros Kovalak <kovalak.maros@gmail.com>
2021-03-14 19:17:53 +0100

    feat: add monitors function

--- cv4/main.py
+++ cv4/main.py
@@ -1,4 +1,6 @@
-from fei.ppds import Mutex
+from fei.ppds import Mutex, Semaphore, print, Event, Thread
+from time import sleep
+from random import randint
 
 
 class LightSwitch(object):
@@ -21,3 +23,23 @@ class LightSwitch(object):
         if self.counter == 0:
             semaphore.signal()
         self.mutex.unlock()
+
+
+class PowerPlant:
+    def __init__(self):
+        self.monitor_ls = LightSwitch()
+        self.no_sensors = Semaphore(1)
+
+    def monitor(self, monitor_id):
+        while True:
+            self.no_monitors.wait()
+
+            number_of_monitors_reading = self.monitor_ls.lock(self.no_sensors)
+            self.no_monitors.signal()
+            duration = randint(40, 50) / 1000
+            print('monit "%02d": '
+                  'pocet_citajucich_monitorov=%02d, '
+                  'trvanie_citania=%03f\n' %
+                  (monitor_id, number_of_monitors_reading, duration*1000))
+            sleep(duration)
+            self.monitor_ls.unlock(self.no_sensors)
