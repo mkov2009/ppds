0f3e5b09122f5b5ba9d33f516537e5ee70e17f70
Maros Kovalak <kovalak.maros@gmail.com>
2021-03-28 22:54:10 +0200

    style: change name of hydrogenCount and oxygenCount

--- cv6/main.py
+++ cv6/main.py
@@ -22,20 +22,20 @@ class Barrier:
 class Shared(object):
     def __init__(self):
         self.mutex = Mutex()
-        self.oxygen = 0
-        self.hydrogen = 0
+        self.oxygenCount = 0
+        self.hydrogenCount = 0
         self.oxyQueue = Semaphore(1)
         self.hydroQueue = Semaphore(2)
         self.barrier = Barrier(3)
 
     def oxygen(self):
         self.mutex.lock()
-        self.oxygen += 1
-        if self.hydrogen < 2:
+        self.oxygenCount += 1
+        if self.hydrogenCount < 2:
             self.mutex.unlock()
         else:
-            self.oxygen -= 1
-            self.hydrogen -= 2
+            self.oxygenCount -= 1
+            self.hydrogenCount -= 2
             self.oxyQueue.signal()
             self.hydroQueue.signal(2)
 
@@ -47,12 +47,12 @@ class Shared(object):
 
     def hydrogen(self):
         self.mutex.lock()
-        self.hydrogen += 1
-        if self.hydrogen < 2 or self.oxygen < 1:
+        self.hydrogenCount += 1
+        if self.hydrogenCount < 2 or self.oxygenCount < 1:
             self.mutex.unlock()
         else:
-            self.oxygen -= 1
-            self.hydrogen -= 2
+            self.oxygenCount -= 1
+            self.hydrogenCount -= 2
             self.oxyQueue.signal()
             self.hydroQueue.signal(2)
 
