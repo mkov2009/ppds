5a6f34743e8f0f8520156bc8a216dbc972b332bf
Maros Kovalak <kovalak.maros@gmail.com>
2021-04-19 22:35:23 +0200

    feat: add fucntion to check ping

--- cv8/sync.py
+++ cv8/sync.py
@@ -1,9 +1,19 @@
 import time
+import os
+
+
+def check_ping(hostname):
+    response = os.system("ping -n 2 " + hostname)
+    if response == 0:
+        ping_status = hostname + " Network Active"
+    else:
+        ping_status = hostname + " Network Error"
+    print(ping_status)
 
 
 def main():
     start = time.time()
-
+    check_ping("google.sk")
     end = time.time()
     print("This task took " + str(end - start) + " seconds to complete.")
 
